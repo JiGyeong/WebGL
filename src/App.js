6b5505f61bbe8ecd827f541dea60f0fff7d4defc
jigyeong <wlrud0126@gmail.com>
2022-02-04 15:23:11 +0900

    deploy project

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import * as THREE from 'three'
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 class App extends Component {
     componentDidMount() {
@@ -12,18 +12,17 @@ class App extends Component {
         renderer.setSize(window.innerWidth, window.innerHeight);
         this.mount.appendChild(renderer.domElement);
         renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight);
-const controls = new OrbitControls(camera, renderer.domElement);
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        const controls = new OrbitControls(camera, renderer.domElement);
 
 
-
-const moonTexture = new THREE.TextureLoader().load('./img/moon.jpg');
-const normalTexture = new THREE.TextureLoader().load('./img/normal.jpg');
-const moon = new THREE.Mesh(
-  new THREE.SphereGeometry(3, 32, 32),
-  new THREE.MeshStandardMaterial({map:moonTexture, normalMap:normalTexture})
-)
-scene.add(moon);
+        const moonTexture = new THREE.TextureLoader().load('./img/moon.jpg');
+        const normalTexture = new THREE.TextureLoader().load('./img/normal.jpg');
+        const moon = new THREE.Mesh(
+            new THREE.SphereGeometry(3, 32, 32),
+            new THREE.MeshStandardMaterial({ map: moonTexture, normalMap: normalTexture })
+        )
+        scene.add(moon);
 
 
         const light = new THREE.HemisphereLight(0xffffbb);
@@ -31,29 +30,29 @@ scene.add(moon);
         camera.position.z = 30;
 
 
-        function addStar(){
+        function addStar() {
             const geomerty = new THREE.SphereGeometry(0.25, 1, 1);
-            const material = new THREE.MeshStandardMaterial({color:0xFFFFFF});
-            const star = new THREE.Mesh(geomerty,material);
-          
-            const [x,y,z] = new Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
-          
-            star.position.set(x,y,z);
-            scene.add(star);
-          }
-          
-          Array(200).fill().forEach(addStar);
+            const material = new THREE.MeshStandardMaterial({ color: 0xFFFFFF });
+            const star = new THREE.Mesh(geomerty, material);
 
-          const spaceTexture = new THREE.TextureLoader().load('./img/space.jpg');
+            const [x, y, z] = new Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+
+            star.position.set(x, y, z);
+            scene.add(star);
+        }
+
+        Array(200).fill().forEach(addStar);
+
+        const spaceTexture = new THREE.TextureLoader().load('./img/space.jpg');
         scene.background = spaceTexture;
 
         const animate = function () {
             requestAnimationFrame(animate);
             moon.rotation.x += 0.01;
-  moon.rotation.y += 0.005;
-  moon.rotation.z += 0.01;
+            moon.rotation.y += 0.005;
+            moon.rotation.z += 0.01;
 
-  controls.update();
+            controls.update();
             renderer.render(scene, camera);
         };
         animate();
